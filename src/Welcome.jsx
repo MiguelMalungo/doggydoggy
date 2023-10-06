3d7266c6296e98f48f37c458e5e0e603ff3c4239
MiguelMalungo <98886465+MiguelMalungo@users.noreply.github.com>
2023-10-06 11:09:09 +0100

    Your commit message here

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import './styles.css';
+import doggybkgVideo from './assets/doggybkg.mp4'; // Importing video from assets folder
 
 function Welcome() {
   return (
     <div className="welcome-page">
       <div className="video-row">
         <video className="background-video" autoPlay loop muted>
-          <source src="/videos/doggybkg.mp4" type="video/mp4" />
+          <source src={doggybkgVideo} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
